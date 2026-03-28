114153694bb99727e5a7051522795d147c1e3f68
Amith <Amith.cabraal@allwyn.co.uk>
2026-03-28 17:38:55 +0000

    Added test flow

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   publicDir: 'public',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        'test-flow': resolve(__dirname, 'test-flow/index.html'),
+      },
+    },
+  },
 })
